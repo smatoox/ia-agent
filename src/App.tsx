94b852620b4cc30d7a4045e472c26215f0484ae4
bart <bc@adversport.com>
2025-02-03 22:11:20 +0100

    Responsive improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import { ButtonNewConversation } from './components/ButtonNewConversation'
 import { ChatBar } from './components/ChatBar'
 import { ConversationList } from './components/ConversationsList'
@@ -5,9 +6,23 @@ import { Dialog } from './components/Dialog'
 import './scss/_app.scss'
 
 function App() {
+  const sidebarRef = useRef<HTMLDivElement>(null)
+  const toggleRef = useRef<HTMLAnchorElement>(null)
+
+  const handleToggle = () => {
+    sidebarRef.current?.classList.toggle('active')
+    toggleRef.current?.classList.toggle('active')
+  }
+
   return (
     <div className="main">
-      <div className='main__sidebar'>
+      <a role="button" className='main__sidebar--toggle' ref={toggleRef} onClick={handleToggle}>
+        <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor">
+          <path d="M13 6L19 12L13 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+          <path d="M5 6L11 12L5 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+      </a>
+      <div className='main__sidebar' ref={sidebarRef}>
         <ConversationList />
       </div>
       <div className='main__dialog'>
